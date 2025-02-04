8ce3cbfe6504fdb1acee16874d3a8cf250c66461
patelmanan4321 <patelmanan4321@gmail.com>
2025-02-04 18:46:21 +0530

    Adding Loader

--- script.js
+++ script.js
@@ -1,33 +1,5 @@
-gsap.from(".bar",{
-    color:"red",
-    duration:1,
-    delay:0.3,
-    opacity:0,
-    stagger:0.3
-})
-
-gsap.from("#title",{
-    color:"red",
-    duration:1,
-    delay:0.2,
-    opacity:0,
-    stagger:0.3
-})
-
-
-gsap.from("#log",{
-    x:1000,
-    duration:0.5,
-    rotate:360,
-    borderRadius:"2px",
-    scale:1
-})
-gsap.to("#log",{
-    x:-1,
-    duration:1,
-    delay:1,
-    rotate:360,
-    borderRadius:"2px",
-    scale:1
-})
-
+window.addEventListener('load', function() {
+    setTimeout(function() {
+        document.getElementById('loader').style.display = 'none';
+    }, 3000);  // 3000 milliseconds = 3 seconds
+});
